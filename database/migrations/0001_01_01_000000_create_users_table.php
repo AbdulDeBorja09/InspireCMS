78f8d374710cfd1c210b6c6b529108d3f77d277d
Abdul De Borja <abduldb09@gmail.com>
2025-02-21 23:28:03 +0800

    Update User Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_type')->default('user');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
