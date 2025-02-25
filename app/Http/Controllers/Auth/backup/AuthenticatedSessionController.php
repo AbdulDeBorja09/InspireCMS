ff27aaf77a836044aef7caa912252f89991bf81b
Abdul De Borja <abduldb09@gmail.com>
2025-02-26 00:07:45 +0800

    fix

--- app/Http/Controllers/Auth/backup/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/backup/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('user.auth.login');
+        return view('User.auth.login');
     }
 
     /**
